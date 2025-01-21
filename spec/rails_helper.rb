c899323f6721b6447e54e81e2e419d93747f7b06
Joe Chirchirillo <cchirchirillo3@gmail.com>
2025-01-20 22:38:18 -0700

    Fix: move shoulda matcher config in rails helper

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,12 +1,5 @@
 require 'spec_helper'
-require 'shoulda/matchers'
-
-Shoulda::Matchers.configure do |config|
-  config.integrate do |with|
-    with.test_framework :rspec
-    with.library :rails
-  end
-end
+# require 'shoulda/matchers'
 
 ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
@@ -74,4 +67,9 @@ RSpec.configure do |config|
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
 end
-
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :rspec
+    with.library :rails
+  end
+end
