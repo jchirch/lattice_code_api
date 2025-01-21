79adead165fefb049d17c1f348469b95d40fe6ae
Joe Chirchirillo <cchirchirillo3@gmail.com>
2025-01-20 22:08:58 -0700

    Chore: almost set up tests, done for night

--- spec/models/course_spec.rb
+++ spec/models/course_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Course, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'associations' do
+    it { should belong_to(:user) }
+    it { should belong_to(:from_language).class_name('Language').with_foreign_key('from_language_id') }
+    it { should belong_to(:to_language).class_name('Language').with_foreign_key('to_language_id') }
+  end
+
+  describe 'validations' do
+    it { should validate_presence_of(:name) }
+  end
 end
