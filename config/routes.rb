c19a6c63afe82cdb6ebf7536f1f0ccc3c89bb40c
Joe Chirchirillo <cchirchirillo3@gmail.com>
2025-02-05 22:27:46 -0700

    Chore: serializer setup

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,22 @@
 Rails.application.routes.draw do
+  resources :languages
+  resources :courses
+  resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
-  resources :users do
-    resources :courses, only: [:index, :show, :create] 
-    resources :user_languages, only: [:index, :create, :update, :destroy] 
+  namespace :api do
+    namespace :v1 do
+      resources :users do
+        resources :courses, only: [:index, :show, :create] 
+        resources :user_languages, only: [:index, :create, :update, :destroy] 
+      end
+    
+      resources :courses, only: [:show, :update, :destroy] 
+      resources :languages, only: [:index, :show] 
+    end
   end
-
-  resources :courses, only: [:show, :update, :destroy] 
-  resources :languages, only: [:index, :show] 
 end
